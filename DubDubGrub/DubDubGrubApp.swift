84b4265cbcf3c352eca164f753de9db8872f31e4
Steven Wijaya <steven.wijaya2001@gmail.com>
2022-12-01 23:12:38 +0700

    Initial Commit

---
+++ DubDubGrub/DubDubGrubApp.swift
@@ -0,0 +1,17 @@
+//
+//  DubDubGrubApp.swift
+//  DubDubGrub
+//
+//  Created by Steven Wijaya on 01.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct DubDubGrubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
