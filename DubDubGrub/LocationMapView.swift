0ed41342897d36cb92d5177cea85ccd09bb1535b
Steven Wijaya <steven.wijaya2001@gmail.com>
2022-12-02 00:08:50 +0700

    Add MapView to project for `LocationMapView`

--- DubDubGrub/LocationMapView.swift
+++ DubDubGrub/LocationMapView.swift
@@ -6,10 +6,40 @@
 //
 
 import SwiftUI
+import MapKit
 
 struct LocationMapView: View {
+    
+    @State private var region = MKCoordinateRegion(
+        center: CLLocationCoordinate2D(
+            latitude: 56.4739,
+            longitude: 84.93953),
+        span: MKCoordinateSpan(
+            latitudeDelta: 0.015,
+            longitudeDelta: 0.015))
+    
     var body: some View {
-        Text("Location Map View")
+        ZStack {
+            mapView
+            logo
+        }
+    }
+}
+
+extension LocationMapView {
+    private var mapView: some View {
+        Map(coordinateRegion: $region)
+            .ignoresSafeArea()
+    }
+    private var logo: some View {
+        VStack {
+            Image("ddg-map-logo")
+                .resizable()
+                .scaledToFit()
+                .frame(height: 75)
+                .shadow(radius: 10)
+            Spacer()
+        }
     }
 }
 
--- README.md
+++ README.md
@@ -3,3 +3,4 @@ This project is taken from Sean Allen courses [DubDubGrub](https://seanallen.tea
 
 ## Timeline:
 1. Project initialization (01.12.2022) ✅
+2. Add MapView using MapKit (02.12.2022) ✅
